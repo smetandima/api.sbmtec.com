45c249aba314a4e4a61483f2a24f6a1573b53024
smetandima <iltw009@gmail.com>
2020-12-08 00:40:45 +1000

    First API add

--- ns/router.js
+++ ns/router.js
@@ -4,12 +4,22 @@ var router = express.Router();
 const bodyParser = require('body-parser')
 const jsonParser = bodyParser.json()
 
+const { getCustomerInfo } = require('./model')
+
 router.get('/', function(req, res) {
   res.send('Root')
 });
 
 router.get('/api', function(req, res) {
   res.send('API')
+});
+
+router.post('/get_customer_info', jsonParser, async function(req, res){
+  await getCustomerInfo(req.body.offset, req.body.limit).then(result => {
+    res.status(200).send(result)
+  }).catch(err => {
+    res.status(500).send('Internal server error')
+  })
 });
 
 // export your router
